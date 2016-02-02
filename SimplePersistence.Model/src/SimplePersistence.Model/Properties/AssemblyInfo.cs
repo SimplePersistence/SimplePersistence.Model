9d8c1b46c3a314ffa50f0b5c6877798d48f07033
João Simões <nogravity00@outlook.com>
2016-02-02 14:03:26 +0000

    Initial project setup

---
+++ SimplePersistence.Model/src/SimplePersistence.Model/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimplePersistence.Model")]
+[assembly: AssemblyDescription("Library that provides consistente model classes guaranteeing ORM compatibility.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Net.JoaoSimoes")]
+[assembly: AssemblyProduct("SimplePersistence")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: Guid("26bf2ceb-dec6-43c8-a94d-6d993ea9ffe6")]
