044049fb51072eb99e739b3eca48482d78a14e66
João Simões <joao.simoes.dev@outlook.pt>
2016-02-02 14:31:44 +0000

    Added IEntity and required dependencies

---
+++ SimplePersistence.Model/src/SimplePersistence.Model/IEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace SimplePersistence.Model
+{
+    /// <summary>
+    /// Represents an entity with a unique identifier
+    /// </summary>
+    /// <typeparam name="TIdentity">The unique identifier type</typeparam>
+    public interface IEntity<TIdentity> : IEntity
+    {
+        TIdentity Id { get; set; }
+    }
+
+    /// <summary>
+    /// Represents an entity
+    /// </summary>
+    public interface IEntity
+    {
+
+    }
+}
