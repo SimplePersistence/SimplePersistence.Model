7e26743ab5b7d9d472500256c98d31de3f19d504
João Simões <joao.simoes.dev@outlook.pt>
2016-02-03 12:21:01 +0000

    Implementations for the deleted metadata

---
+++ SimplePersistence.Model/src/SimplePersistence.Model/EntityWithLocalDeletedMetaAndVersionAsByteArray.cs
@@ -0,0 +1,72 @@
+﻿#region License
+// The MIT License (MIT)
+// 
+// Copyright (c) 2016 SimplePersistence
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+#endregion
+namespace SimplePersistence.Model
+{
+    using System;
+
+    /// <summary>
+    /// Represents an entity that has an unique identifier, local deleted metadata and version info, 
+    /// using a byte[] for the <see cref="IHaveVersion{T}.Version"/>.
+    /// </summary>
+    /// <typeparam name="TIdentity">The identifier type</typeparam>
+    /// <typeparam name="TDeletedBy">The deleted by type</typeparam>
+    public abstract class EntityWithLocalDeletedMetaAndVersionAsByteArray<TIdentity, TDeletedBy>
+        : Entity<TIdentity>, IHaveLocalDeletedMeta<TDeletedBy>, IHaveVersion<byte[]>
+    {
+        #region Implementation of IHaveLocalDeletedMeta<TDeletedBy>
+
+        /// <summary>
+        /// The <see cref="DateTime"/> when it was soft deleted
+        /// </summary>
+        public virtual DateTime? DeletedOn { get; set; }
+
+        /// <summary>
+        /// The identifier (or entity) which soft deleted this entity
+        /// </summary>
+        public virtual TDeletedBy DeletedBy { get; set; }
+
+        #endregion
+
+        #region Implementation of IHaveVersion<TVersion>
+
+        /// <summary>
+        /// The entity version
+        /// </summary>
+        public virtual byte[] Version { get; set; }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Represents an entity that has an unique identifier, deleted metadata and version info, 
+    /// using a byte[] for the <see cref="IHaveVersion{T}.Version"/> and a <see cref="string"/> as an 
+    /// identifier for the <see cref="IHaveLocalDeletedMeta{T}.DeletedBy"/>
+    /// </summary>
+    /// <typeparam name="TIdentity">The identifier type</typeparam>
+    public abstract class EntityWithLocalDeletedMetaAndVersionAsByteArray<TIdentity>
+        : EntityWithLocalDeletedMetaAndVersionAsByteArray<TIdentity, string>, IHaveLocalDeletedMeta
+    {
+
+    }
+}
