0e1cd774521a32b0b05a26ce39a2c99da8083953
João Simões <joao.simoes.dev@outlook.pt>
2016-02-03 10:42:10 +0000

    New entities

---
+++ SimplePersistence.Model/src/SimplePersistence.Model/Entity.cs
@@ -0,0 +1,22 @@
+﻿namespace SimplePersistence.Model
+{
+    /// <summary>
+    /// Represents an entity with a unique identifier
+    /// </summary>
+    /// <typeparam name="TIdentity">The unique identifier type</typeparam>
+    public abstract class Entity<TIdentity> : IEntity<TIdentity>
+    {
+        /// <summary>
+        /// Unique identifier
+        /// </summary>
+        public virtual TIdentity Id { get; set; }
+    }
+
+    /// <summary>
+    /// Represents an entity without any unique identifier
+    /// </summary>
+    public abstract class Entity : IEntity
+    {
+
+    }
+}
