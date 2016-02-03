6d063942a54ed4e44001f65feb79f472a0ae99fb
João Simões <joao.simoes.dev@outlook.pt>
2016-02-03 11:50:04 +0000

    Created and updated model implementations

---
+++ SimplePersistence.Model/src/SimplePersistence.Model/EntityWithLocalCreatedAndUpdatedMeta.cs
@@ -0,0 +1,108 @@
+﻿#region License
+// The MIT License (MIT)
+// 
+// Copyright (c) 2016 SimplePersistence
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+#endregion
+namespace SimplePersistence.Model
+{
+    using System;
+
+    /// <summary>
+    /// Represents an entity that has an unique identifier, created and updated metadata
+    /// </summary>
+    /// <typeparam name="TIdentity">The identifier type</typeparam>
+    /// <typeparam name="TCreatedBy">The created by type</typeparam>
+    /// <typeparam name="TUpdatedBy">The updated by type</typeparam>
+    public abstract class EntityWithLocalCreatedAndUpdatedMeta<TIdentity, TCreatedBy, TUpdatedBy>
+        : Entity<TIdentity>, IHaveLocalCreatedMeta<TCreatedBy>, IHaveLocalUpdatedMeta<TUpdatedBy>
+    {
+        private DateTime _createdOn;
+        private DateTime _updatedOn;
+
+        #region Implementation of IHaveLocalCreatedMeta<TCreatedBy>
+
+        /// <summary>
+        /// The <see cref="DateTime"/> when it was created
+        /// </summary>
+        public virtual DateTime CreatedOn
+        {
+            get { return _createdOn; }
+            set { _createdOn = value; }
+        }
+
+        /// <summary>
+        /// The identifier (or entity) which first created this entity
+        /// </summary>
+        public virtual TCreatedBy CreatedBy { get; set; }
+
+        #endregion
+
+        #region Implementation of IHaveLocalUpdatedMeta<TUpdatedBy>
+
+        /// <summary>
+        /// The <see cref="DateTime"/> when it was last updated
+        /// </summary>
+        public virtual DateTime UpdatedOn
+        {
+            get { return _updatedOn; }
+            set { _updatedOn = value; }
+        }
+
+        /// <summary>
+        /// The identifier (or entity) which last updated this entity
+        /// </summary>
+        public virtual TUpdatedBy UpdatedBy { get; set; }
+
+        #endregion
+
+        /// <summary>
+        /// Creates a new instance and sets the <see cref="CreatedOn"/> and 
+        /// <see cref="UpdatedOn"/> to <see cref="DateTime.Now"/>
+        /// </summary>
+        protected EntityWithLocalCreatedAndUpdatedMeta()
+        {
+            _createdOn = _updatedOn = DateTime.Now;
+        }
+    }
+
+    /// <summary>
+    /// Represents an entity that has an unique identifier, created and updated metadata
+    /// </summary>
+    /// <typeparam name="TIdentity">The identifier type</typeparam>
+    /// <typeparam name="TCreatedAndUpdated">The created and updated by type</typeparam>
+    public abstract class EntityWithLocalCreatedAndUpdatedMeta<TIdentity, TCreatedAndUpdated>
+        : EntityWithLocalCreatedAndUpdatedMeta<TIdentity, TCreatedAndUpdated, TCreatedAndUpdated>
+    {
+
+    }
+
+    /// <summary>
+    /// Represents an entity that has an unique identifier, created and updated metadata,
+    /// using a <see cref="string"/> as an identifier for the <see cref="IHaveLocalCreatedMeta{T}.CreatedBy"/>
+    /// and <see cref="IHaveLocalUpdatedMeta{T}.UpdatedBy"/>
+    /// </summary>
+    /// <typeparam name="TIdentity">The identifier type</typeparam>
+    public abstract class EntityWithLocalCreatedAndUpdatedMeta<TIdentity>
+        : EntityWithLocalCreatedAndUpdatedMeta<TIdentity, string, string>, IHaveLocalCreatedMeta, IHaveLocalUpdatedMeta
+    {
+
+    }
+}
---
+++ SimplePersistence.Model/src/SimplePersistence.Model/EntityWithLocalCreatedAndUpdatedMetaAndVersionAsByteArray.cs
@@ -0,0 +1,120 @@
+﻿#region License
+// The MIT License (MIT)
+// 
+// Copyright (c) 2016 SimplePersistence
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+#endregion
+namespace SimplePersistence.Model
+{
+    using System;
+
+    /// <summary>
+    /// Represents an entity that has an unique identifier, local created, updated metadata and version info, 
+    /// using a byte[] for the <see cref="IHaveVersion{T}.Version"/>.
+    /// </summary>
+    /// <typeparam name="TIdentity">The identifier type</typeparam>
+    /// <typeparam name="TCreatedBy">The created by type</typeparam>
+    /// <typeparam name="TUpdatedBy">The updated by type</typeparam>
+    public abstract class EntityWithLocalCreatedAndUpdatedMetaAndVersionAsByteArray<TIdentity, TCreatedBy, TUpdatedBy>
+        : Entity<TIdentity>, IHaveLocalCreatedMeta<TCreatedBy>, IHaveLocalUpdatedMeta<TUpdatedBy>, IHaveVersion<byte[]>
+    {
+        private DateTime _createdOn;
+        private DateTime _updatedOn;
+
+        #region Implementation of IHaveLocalCreatedMeta<TCreatedBy>
+
+        /// <summary>
+        /// The <see cref="DateTime"/> when it was created
+        /// </summary>
+        public virtual DateTime CreatedOn
+        {
+            get { return _createdOn; }
+            set { _createdOn = value; }
+        }
+
+        /// <summary>
+        /// The identifier (or entity) which first created this entity
+        /// </summary>
+        public virtual TCreatedBy CreatedBy { get; set; }
+
+        #endregion
+
+        #region Implementation of IHaveLocalUpdatedMeta<TUpdatedBy>
+
+        /// <summary>
+        /// The <see cref="DateTime"/> when it was last updated
+        /// </summary>
+        public virtual DateTime UpdatedOn
+        {
+            get { return _updatedOn; }
+            set { _updatedOn = value; }
+        }
+
+        /// <summary>
+        /// The identifier (or entity) which last updated this entity
+        /// </summary>
+        public virtual TUpdatedBy UpdatedBy { get; set; }
+
+        #endregion
+
+        #region Implementation of IHaveVersion<TVersion>
+
+        /// <summary>
+        /// The entity version
+        /// </summary>
+        public virtual byte[] Version { get; set; }
+
+        #endregion
+
+        /// <summary>
+        /// Creates a new instance and sets the <see cref="CreatedOn"/> and 
+        /// <see cref="UpdatedOn"/> to <see cref="DateTime.Now"/>
+        /// </summary>
+        protected EntityWithLocalCreatedAndUpdatedMetaAndVersionAsByteArray()
+        {
+            _createdOn = _updatedOn = DateTime.Now;
+        }
+    }
+
+    /// <summary>
+    /// Represents an entity that has an unique identifier, created, updated metadata and version info, 
+    /// using a byte[] for the <see cref="IHaveVersion{T}.Version"/>.
+    /// </summary>
+    /// <typeparam name="TIdentity">The identifier type</typeparam>
+    /// <typeparam name="TCreatedAndUpdated">The created, updated and deleted by type</typeparam>
+    public abstract class EntityWithLocalCreatedAndUpdatedMetaAndVersionAsByteArray<TIdentity, TCreatedAndUpdated>
+        : EntityWithLocalCreatedAndUpdatedMetaAndVersionAsByteArray<TIdentity, TCreatedAndUpdated, TCreatedAndUpdated>
+    {
+
+    }
+
+    /// <summary>
+    /// Represents an entity that has an unique identifier, created, updated, deleted metadata and version info, 
+    /// using a byte[] for the <see cref="IHaveVersion{T}.Version"/> and a <see cref="string"/> as an 
+    /// identifier for the <see cref="IHaveLocalCreatedMeta{T}.CreatedBy"/>,
+    /// <see cref="IHaveLocalUpdatedMeta{T}.UpdatedBy"/> and <see cref="IHaveLocalDeletedMeta{T}.DeletedBy"/>
+    /// </summary>
+    /// <typeparam name="TIdentity">The identifier type</typeparam>
+    public abstract class EntityWithLocalCreatedAndUpdatedMetaAndVersionAsByteArray<TIdentity>
+        : EntityWithLocalCreatedAndUpdatedMetaAndVersionAsByteArray<TIdentity, string, string>, IHaveLocalCreatedMeta, IHaveLocalUpdatedMeta
+    {
+
+    }
+}
