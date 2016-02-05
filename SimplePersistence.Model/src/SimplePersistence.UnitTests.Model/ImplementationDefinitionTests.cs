727c1f3ba52333c59f240b6810327dbb3936c5ac
João Simões <joao.simoes.dev@outlook.pt>
2016-02-05 14:27:06 +0000

    Configured unit tests per platform

---
+++ SimplePersistence.Model/src/SimplePersistence.UnitTests.Model/ImplementationDefinitionTests.cs
@@ -0,0 +1,17 @@
+﻿namespace SimplePersistence.UnitTests.Model
+{
+#if !DNXCORE50 && !DNX451
+    using Fact = NUnit.Framework.TestAttribute;
+#else
+    using Xunit;
+#endif
+
+    public class ImplementationDefinitionTests
+    {
+        [Fact]
+        public void AllPropertiesMustBeVirtual()
+        {
+            
+        }
+    }
+}
