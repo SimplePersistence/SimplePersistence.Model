9cc0f0d63321d51692d5ef6ec03928c389aa7080
João Simões <joao.simoes.dev@outlook.pt>
2016-02-05 12:16:32 +0000

    Added project for unit tests

---
+++ SimplePersistence.Model/src/SimplePersistence.UnitTests.Model/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SimplePersistence.UnitTests.Model")]
+[assembly: AssemblyDescription("Unit test library for SimplePersistence.Model")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Net.JoaoSimoes")]
+[assembly: AssemblyProduct("SimplePersistence")]
+[assembly: AssemblyCopyright("Copyright ©  2016 SimplePersistence")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+#if !(portable40 || portable)
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("26bf2ceb-dec6-43c8-a94d-6d993ea9ffe6")]
+
+#endif
+
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
