673c71dcd06ed5db3a6f8bbd326321d13f453c9f
João Simões <joao.simoes.dev@outlook.pt>
2016-02-28 00:48:59 +0000

    Unified assembly description for all platforms

--- SimplePersistence.Model/src/SimplePersistence.Model/Properties/AssemblyInfo.cs
+++ SimplePersistence.Model/src/SimplePersistence.Model/Properties/AssemblyInfo.cs
@@ -26,21 +26,7 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-#if PORTABLE40
-[assembly: AssemblyTitle("SimplePersistence.Model Portable .NET 4.0")]
-#elif NETCORE45
-[assembly: AssemblyTitle("SimplePersistence.Model Portable")]
-#elif DOTNET
-[assembly: AssemblyTitle("SimplePersistence.Model .NET Platform")]
-#elif NET20
-[assembly: AssemblyTitle("SimplePersistence.Model .NET 2.0")]
-#elif NET35
-[assembly: AssemblyTitle("SimplePersistence.Model .NET 3.5")]
-#elif NET40
-[assembly: AssemblyTitle("SimplePersistence.Model .NET 4.0")]
-#else
 [assembly: AssemblyTitle("SimplePersistence.Model")]
-#endif
 
 [assembly: AssemblyDescription("Library with helper interfaces and classes to provide consistency across application models. Implementations are ORM compatible and should be usable in most databases.")]
 [assembly: AssemblyConfiguration("")]
