1fa028311cad699b9803d172646dfb426701c7a6
João Simões <joao.simoes.dev@outlook.pt>
2016-03-31 11:13:58 +0100

    Missing IsDeleted extensions

--- SimplePersistence.Model/src/SimplePersistence.Model/Helper/ModelsExtensions-IsDeleted.cs
+++ SimplePersistence.Model/src/SimplePersistence.Model/Helper/ModelsExtensions-IsDeleted.cs
@@ -34,43 +34,62 @@ namespace SimplePersistence.Model.Helper
 #if !NET20
 
         /// <summary>
-        /// Checks who deleted the <see cref="IHaveDeletedMeta{TDeletedBy}"/> instance.
+        /// Checks if the <see cref="IHaveDeletedMeta{TDeletedBy}"/> instance is deleted.
         /// </summary>
         /// <param name="entity">The entity to check</param>
-        /// <param name="by">Who deleted the entity</param>
         /// <typeparam name="T">The entity type</typeparam>
         /// <typeparam name="TBy">The deleted by type</typeparam>
-        /// <returns>True if the instance was deleted by the given parameter</returns>
+        /// <returns>True if the instance is deleted</returns>
         /// <exception cref="ArgumentNullException"/>
-        public static bool WasDeletedBy<T, TBy>(this T entity, TBy by)
+        public static bool IsDeleted<T, TBy>(this T entity)
             where T : IHaveDeletedMeta<TBy>
+            where TBy : class 
         {
             if (entity == null) throw new ArgumentNullException(nameof(entity));
 
-            return entity.DeletedBy.Equals(by);
+            return entity.DeletedBy == null;
         }
 
 #endif
 
         /// <summary>
-        /// Checks who deleted the <see cref="IHaveLocalDeletedMeta{TDeletedBy}"/> instance.
+        /// Checks if the <see cref="IHaveLocalDeletedMeta{TDeletedBy}"/> instance is deleted.
         /// </summary>
         /// <param name="entity">The entity to check</param>
-        /// <param name="by">Who deleted the entity</param>
         /// <typeparam name="T">The entity type</typeparam>
         /// <typeparam name="TBy">The deleted by type</typeparam>
-        /// <returns>True if the instance was deleted by the given parameter</returns>
+        /// <returns>True if the instance is deleted</returns>
         /// <exception cref="ArgumentNullException"/>
 #if NET20
-        public static bool WasDeletedLocallyBy<T, TBy>(T entity, TBy by)
+        public static bool IsDeletedLocally<T, TBy>(T entity)
 #else
-        public static bool WasDeletedLocallyBy<T, TBy>(this T entity, TBy by)
+        public static bool IsDeletedLocally<T, TBy>(this T entity)
 #endif
             where T : IHaveLocalDeletedMeta<TBy>
+            where TBy : class
         {
             if (entity == null) throw new ArgumentNullException(nameof(entity));
 
-            return entity.DeletedBy.Equals(by);
+            return entity.DeletedBy == null;
+        }
+
+        /// <summary>
+        /// Checks if the <see cref="IHaveSoftDelete"/> instance is deleted.
+        /// </summary>
+        /// <param name="entity">The entity to check</param>
+        /// <typeparam name="T">The entity type</typeparam>
+        /// <returns>True if the instance is deleted</returns>
+        /// <exception cref="ArgumentNullException"/>
+#if NET20
+        public static bool IsDeleted<T>(T entity)
+#else
+        public static bool IsDeleted<T>(this T entity)
+#endif
+            where T : IHaveSoftDelete
+        {
+            if (entity == null) throw new ArgumentNullException(nameof(entity));
+
+            return entity.Deleted;
         }
     }
 }
